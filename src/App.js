d2499f697d94cd70cfe5537fd2a3ed66f26fc9a2
secretivo <secretivo@secretivos-Air.lan>
2023-05-11 22:13:23 -0400

    assignment in class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import People from "./pages/People";
+import NewPerson from "./pages/NewPerson";
+import Person from "./pages/Person";
+import UpdatePerson from "./pages/UpdatePerson";
+import Navbar from "./components/Navbar";
 
+//container comp
 function App() {
+  let routes;
+
+  routes = (
+    <Routes>
+      <Route exact={true} path="/" element={<People />} />
+      <Route path="/new" element={<NewPerson />} />
+      <Route path="/:id" element={<Person />} />
+      <Route path="/:id/edit" element={<UpdatePerson />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      App.js
+      <header>
+        <Navbar />
       </header>
+      <main>{routes}</main>
     </div>
   );
 }
 
 export default App;
+
+
